822ca49bdaf1b2b50b6888365132d8fb0d4128da
Emmanuel Akintunde <nerad9@gmail.com>
2026-02-19 09:00:11 +0100

    Align Pages base with repo casing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Use repo name in lowercase to match GitHub Pages URL path.
-  base: '/moviearc/',
+  // Use repo name casing to match GitHub Pages path.
+  base: '/MovieArc/',
   plugins: [react()],
 })
